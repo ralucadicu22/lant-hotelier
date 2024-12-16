fb68f9b2874d23cfaf038d0dcf5f9a66ff76b33c
Raluca <dicuraluca22@yahoo.com>
2024-12-16 20:24:06 +0200

    Initial commit

---
+++ src/main/java/com/example/lant_hotelier/LantHotelierApplication.java
@@ -0,0 +1,13 @@
+package com.example.lant_hotelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LantHotelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LantHotelierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lant_hotelier/LantHotelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lant_hotelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LantHotelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
